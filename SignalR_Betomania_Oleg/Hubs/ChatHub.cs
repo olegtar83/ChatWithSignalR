c3a53798f5d1026bad0a71f108733ae1ae620395
olegtar83 <olegtar83@gmail.com>
2018-12-01 17:14:53 +0200

    Add project files.

---
+++ SignalR_Betomania_Oleg/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+
+    public class ChatHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public void Send(string name, string message, string connID)
+        {
+             // Call the broadcastMessage method to update clients.
+              Clients.Group(name).ReceiveMessage(message);
+           
+        }
+
+        public void AddGroup(string group, string connID)
+        {
+            Groups.Add(connID, group);
+        }
+    
+
+
+    }
+}
---
+++ SignalR_Betomania_Oleg/Scripts/angularScripts/controllers.js
@@ -0,0 +1,52 @@
+﻿var myApp = angular.module('myApp',[]);
+myApp.controller('SignalRCtrl', function  ($scope,$window) {
+
+    $scope.init = function () {
+
+        $window.localStorage.clear();
+        $scope.chatButton = 'lobby';
+        $scope.switch=function () {
+            cleanChat();
+            startChat();
+        }
+        var startChat = function () {
+            $.connection.hub.start().done(function () {
+                var groupName =$scope.chatButton;
+                chat.server.addGroup(groupName, $.connection.hub.id).then(() => {
+                    console.log('Switched channels ' + groupName);
+                });
+                var chatHistory = $window.localStorage.getItem(groupName);
+                if (chatHistory !== null) {
+                    $('#chat').append(chatHistory);
+                }
+                $scope.sendMessage = function(){
+                    // Call the Send method on the hub.
+                    chat.server.send(groupName, $scope.message, $.connection.hub.id);
+                    $scope.message = '';
+                    // Clear text box and reset focus for next comment.
+                }
+            });
+        };
+        startChat();
+        var chat = $.connection.chatHub;
+
+        chat.client.receiveMessage = function (message) {
+            var groupName = $scope.chatButton;
+            var newMessage = '\n' + "me : " + message;
+            $('#chat').append(newMessage);
+           
+            var previousHistory = $window.localStorage.getItem(groupName) || '';
+            $window.localStorage.setItem(groupName, previousHistory + newMessage)
+        };
+
+        function cleanChat() {
+             $('#chat').empty();
+          
+        }
+
+
+
+    };
+
+
+})
